0a422908df4df22f79750fe456d6fddd066b454c
Daniel Andrade <daniel.andrade@sga.pucminas.br>
2018-08-11 17:18:36 -0300

    Formulario alteração CSS

--- vscexemplo/Sorteio_de_Cupons/script.js
+++ vscexemplo/Sorteio_de_Cupons/script.js
@@ -1,13 +1,37 @@
 function myFunction() {
     var x = Math.floor((Math.random() * 10) + 1);
     var greeting;
-    if(x ==2){
+
+    alert("clique para abrir o pacote");
+
+    if (x == 2) {
         greeting = "Parabéns você ganhou cupom de 10% codigo: #desonto10";
-    
+
     }
-    else{
+    else {
         greeting = " Infelizmente não foi desta vez, quer tentar novamente preencha os campos"
     }
     document.getElementById("demo").innerHTML = greeting;
-    
+
+}
+
+
+function salvar() {
+// função mostrar botão
+    $(document).ready(function () {
+        $("#hide").click(function () {
+           
+            $(hide).show();
+
+        });
+    });
+    document.getElementById('conteudo').document.designMode = 'On';
+    var conteudo = '';
+
+    conteudo += "Nome: " + document.getElementById('nome').value + "<br>";
+    conteudo += "Sobrenome: " + document.getElementById('sobrenome').value + "<br>";
+
+    frames['conteudo'].document.body.innerHTML = conteudo;
+
+    frames['conteudo'].document.execCommand('SaveAs', false, 'arquivo.txt');
 }
